a20ac786361fca9d0bcb47065aab80dcb0805fa4
83hrfjsdfjshi <143231894+83hrfjsdfjshi@users.noreply.github.com>
2023-08-25 15:02:56 -1000

    wod euler problem 1

---
+++ e08 euler problem/attempt1.js
@@ -0,0 +1,11 @@
+function projectEulerOne() {
+    const multiples = [3, 5];
+    const findBelow = 1000;
+    let sum = 0;
+    for (let i = 0; i < findBelow; i++) {
+        if (i % 3 === 0 || i % 5 === 0) sum += i;
+    }
+    return sum;
+}
+
+console.log(projectEulerOne());
